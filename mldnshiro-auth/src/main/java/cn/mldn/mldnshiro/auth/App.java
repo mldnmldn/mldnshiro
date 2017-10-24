4677874dfa9e5720eddbe9962373d371c3a697c5
mldn <mldnqa@163.com>
2017-10-24 11:32:44 +0800

    创建mldnshiro-auth模块讲解认证与授权处理操作。

---
+++ mldnshiro-auth/src/main/java/cn/mldn/mldnshiro/auth/App.java
@@ -0,0 +1,17 @@
+package cn.mldn.mldnshiro.auth;
+
+import org.apache.shiro.realm.jdbc.JdbcRealm;
+
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
